831a80e9687e1b6ff3a82efac9ee74fd468a24f8
Almaz <Zickam@mail.ru>
2024-10-26 18:41:17 +0500

    initial comit

---
+++ common_installs.sh
@@ -0,0 +1,32 @@
+apt install sudo -y
+apt install git -y
+apt install ufw -y
+apt install htop -y
+ufw allow 22
+ufw allow 443
+
+### Docker installation
+# https://docs.docker.com/engine/install/debian/#installation-methods
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+### Docker installation
+
+ufw enable
+
+< /dev/zero ssh-keygen -q -N ""
+echo "Auto executed ssh-keygen output:
+" && cat .ssh/id_rsa.pub
